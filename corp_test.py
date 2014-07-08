fa3fc823d19a0d18b92e42967434bd129138cb57
Greg Taschuk <greg.taschuk@originate.com>
2014-07-08 17:36:16 -0400

    added a model for a salaried corporation

--- corp_test.py
+++ corp_test.py
@@ -1,19 +1,19 @@
 import pyethereum
+import pytest
 
 t = pyethereum.tester
 s = t.state()
-c = s.contract("corp.se")
+c = s.contract("salaried_corp.se")
 
-def initialize_contract(ether):
+def initialize_contract(key, ether):
     s.mine(1)
-    s.send(t.k2, c, ether)
     print "Initialized contract with %i ether" % ether
-
+    return s.send(key, c, ether)
 
 def transfer(key, sh_address, num_shares):
     s.mine(1)
-    print s.send(key, c, 0, [1, sh_address, num_shares])
     print "\nTransfered %i shares..." % num_shares
+    return s.send(key, c, 0, [1, sh_address, num_shares])
 
 
 def check_share_balances():
@@ -23,42 +23,50 @@ def check_share_balances():
             s.send(t.k2, c, 0, [2])[0],
             s.send(t.k1, c, 0, [2])[0])
 
-def pay_dividends():
-    div = s.send(t.k2, c, 0, [3])
+def pay_dividends(admin_key):
+    div = s.send(admin_key, c, 0, [3])
     s.mine(1)
     print "\nPaid %i in dividends to shareholders" % div[0]
 
+def pay_salaries(admin_key):
+    s.mine(1)
+    return s.send(admin_key, c, 0, [6])
+
 def make_profit(ether):
     s.mine(1)
-    s.send(t.k9, c, ether, [3])
     print "\nMade %i profit" % ether
+    return s.send(t.k9, c, ether, [3])
 
 def print_account_balances():
     print "Account balances (Ether)"
     print "%i\t%i" % (
             s.block.get_balance(t.a2) - initial[0],
             s.block.get_balance(t.a1) - initial[1])
-    
-initialize_contract(10**9)
-transfer(t.k2, t.a1, 30000)
-initial = [
-    s.block.get_balance(t.a2),
-    s.block.get_balance(t.a1)]
+ 
+def set_salary(key, worker_address, daily_salary):
+    s.mine(1)
+    return s.send(key, c, 0, [4, worker_address, daily_salary])
 
-check_share_balances()
-print_account_balances()
+def check_salary(key):
+    s.mine(1)
+    return s.send(t.k2, c, 0, [7])
 
-pay_dividends()
-print_account_balances()
+initialize_contract(t.k9, 10**9)
+set_salary(t.k9, t.a2, 10000)
+set_salary(t.k9, t.a3, 10000)
 
-make_profit(200000000)
-pay_dividends()
-print_account_balances()
+salary = check_salary(t.k2)[0]
+assert salary == 10000
 
+bal_before = pay_salaries(t.k9)
+s.mine(61)
+bal_after = pay_salaries(t.k9)
+print bal_before
+print bal_after
+assert (bal_before[0] - bal_after[0]) == 2*salary
 
-transfer(t.k1, t.a2, 10000)
-check_share_balances()
-make_profit(100000000)
-pay_dividends()
-print_account_balances()
-
+print pay_salaries(t.k9)
+s.mine(61)
+print pay_salaries(t.k9)
+s.mine(61)
+print pay_salaries(t.k9)
